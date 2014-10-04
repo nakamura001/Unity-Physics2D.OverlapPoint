e0250d8f7991bddd8d4de8b00024392feefca6e3
nakamura001 <polluxtk@gmail.com>
2014-10-04 22:53:28 +0900

    プロジェクトをコミット。

---
+++ Assets/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+	public Text infoText;
+
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+		Vector3 dir = -gameObject.transform.right;
+		Collider2D collider = null;
+		if (Input.GetKey(KeyCode.Mouse0)) {
+			Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			// public static Collider2D OverlapPoint(Vector2 point, int layerMask = DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity); 
+			collider = Physics2D.OverlapPoint(new Vector2(point.x, point.y));
+		}
+		if (collider) {
+			infoText.text = collider.name;
+		} else {
+			infoText.text = "No data";
+		}
+	}
+}
